8b16385d990731a03f13b2523a4a920eb289cad0
PrjGaikwad <gaikwadprj2612@gmail.com>
2026-06-20 19:47:39 -0700

    new class addded

---
+++ DemoProj/src/com/demo/Test/printArray.java
@@ -0,0 +1,12 @@
+package com.demo.Test;
+
+public class printArray {
+
+	public static void main(String[] args) {
+		int arr[] = {3445,45,45,34,34};
+		
+		for(int x: arr) {
+			System.out.print(x+" ");
+		}
+	}
+}
